ab3c4ebe65a37be971740aafdac47d4dea6e4d28
Sam <81268132+Sackah@users.noreply.github.com>
2023-11-18 19:16:55 +0000

    changed unit to vh

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,19 @@
 import SideNav from "../components/SideNav/SideNav";
 import TopNav from "../components/TopNav/TopNav";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div className="home-page">
+    <>
       <TopNav />
       <SideNav />
-    </div>
+      <div className="home-page">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+        tenetur quis aperiam obcaecati. Dolorem nobis esse sequi exercitationem,
+        mollitia sed saepe officia quia numquam voluptatem? Possimus
+        perspiciatis impedit delectus corrupti?
+      </div>
+    </>
   );
 };
 
